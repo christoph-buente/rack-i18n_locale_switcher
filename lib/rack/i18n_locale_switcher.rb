155b467971c625a42275a6b4849bc1d510e49108
Christoph Bünte <info@christophbuente.de>
2011-11-24 17:25:33 +0100

    Use syntactic sure to make code like nice :)

--- lib/rack/i18n_locale_switcher.rb
+++ lib/rack/i18n_locale_switcher.rb
@@ -20,7 +20,7 @@ module Rack
     private
 
     def is_available?(locale)
-      (locale.nil? ||locale == '') ? false : I18n.available_locales.include?(locale.to_sym)
+      not locale.nil? and not locale.empty? and I18n.available_locales.include?(locale.to_sym)
     end
 
     def extract_locale_from_params(request)
--- rack-i18n_locale_switcher.gemspec
+++ rack-i18n_locale_switcher.gemspec
@@ -2,7 +2,7 @@
 
 Gem::Specification.new do |s|
   s.name = "rack-i18n_locale_switcher"
-  s.version = "0.2.2"
+  s.version = "0.2.3"
 
   s.required_rubygems_version = Gem::Requirement.new(">= 1.2") if s.respond_to? :required_rubygems_version=
   s.authors = ["Christoph B\u{fc}nte, Andreas Korth"]
